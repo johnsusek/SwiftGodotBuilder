6ec0af3246bb8dd0c870e2609094393074ad6aa1
John Susek <john@johnsolo.net>
2025-09-05 11:18:47 -0500

    Add HUD example

--- Examples/Sources/SwiftGodotBuilderExampleApp.swift
+++ Examples/Sources/SwiftGodotBuilderExampleApp.swift
@@ -24,6 +24,8 @@ struct SwiftGodotBuilderExampleApp: App {
     let view = PongView()
 //    let view = BreakoutView()
 //    let view = SpaceInvadersView()
+//    let view = HUDView()
+
     let node = view.toNode()
 
     sceneTree.root?.addChild(node: node)
@@ -41,7 +43,7 @@ struct SwiftGodotBuilderExampleApp: App {
 
 private func pollForSceneTree() async -> SceneTree {
   for _ in 0 ..< 300 {
-    if let t = Engine.getMainLoop() as? SceneTree { return t }
+    if let tree = Engine.getSceneTree() { return tree }
     try? await Task.sleep(nanoseconds: 200_000_000)
   }
   fatalError("No scene tree or scene root - cannot continue!")
