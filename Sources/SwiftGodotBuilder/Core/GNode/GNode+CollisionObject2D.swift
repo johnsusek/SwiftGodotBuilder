e25bdf5161531477f7533004a4c7520038d0f76f
John Susek <john@johnsolo.net>
2025-09-07 19:39:05 -0500

    mask -> collisionMask, layer -> collisionLayer

--- Sources/SwiftGodotBuilder/Core/GNode/GNode+CollisionObject2D.swift
+++ Sources/SwiftGodotBuilder/Core/GNode/GNode+CollisionObject2D.swift
@@ -9,13 +9,13 @@ import SwiftGodot
 ///   .mask([.gamma])
 /// ```
 public extension GNode where T: CollisionObject2D {
-  func layers(_ set: Physics2DLayer) -> Self {
+  func collisionLayer(_ set: Physics2DLayer) -> Self {
     var s = self
     s.ops.append { $0.collisionLayer = UInt32(set.rawValue) }
     return s
   }
 
-  func mask(_ set: Physics2DLayer) -> Self {
+  func collisionMask(_ set: Physics2DLayer) -> Self {
     var s = self
     s.ops.append { $0.collisionMask = UInt32(set.rawValue) }
     return s
