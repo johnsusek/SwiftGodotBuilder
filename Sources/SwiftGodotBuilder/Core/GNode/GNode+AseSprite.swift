10bb1c0bbe6eb6a13e0f491f165d90ed6a112354
John Susek <john@johnsolo.net>
2025-09-07 19:38:41 -0500

    Move Ase helper into core

---
+++ Sources/SwiftGodotBuilder/Core/GNode/GNode+AseSprite.swift
@@ -0,0 +1,30 @@
+import Foundation
+import SwiftGodot
+
+// MARK: - Extension for GNode builder
+
+public extension GNode where T == AseSprite {
+  /// Convenience initializer for creating an `AseSprite` within a `GNode` builder context.
+  ///
+  /// Example:
+  /// ```swift
+  /// let dinoView = GNode<AseSprite>(path: "DinoSprites", layer: "MORT")
+  /// ```
+  init(
+    _ name: String? = UUID().uuidString,
+    path: String,
+    layer: String? = nil,
+    options: AseOptions = .init(),
+    autoplay: String? = nil,
+    @NodeBuilder _ children: () -> [any GView] = { [] }
+  ) {
+    self.init(name, children, make: {
+      let a = T()
+      a.sourcePath = path
+      a.layerName = layer
+      a.aseOptions = options
+      a.autoplayAnimation = autoplay
+      return a
+    })
+  }
+}
