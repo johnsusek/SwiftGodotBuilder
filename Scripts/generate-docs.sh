63f291bc9c8fe0f8ea58a761e3bed3b549a62fcc
John Susek <john@johnsolo.net>
2025-09-09 09:11:36 -0500

    Update script to generate docs for both modules

--- Scripts/generate-docs.sh
+++ Scripts/generate-docs.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-swift package --allow-writing-to-directory docs generate-documentation --target $1 --include-extended-types --transform-for-static-hosting --hosting-base-path $1 --output-path docs
+swift package --allow-writing-to-directory "docs" generate-documentation --target SwiftGodotBuilder --include-extended-types --transform-for-static-hosting --hosting-base-path "SwiftGodotBuilder" --output-path "docs"
+
+swift package --allow-writing-to-directory "docs" generate-documentation --target SwiftGodotPatterns --include-extended-types --transform-for-static-hosting --hosting-base-path "SwiftGodotBuilder" --output-path "docs/SwiftGodotPatterns"
+
+# Post-process the generated docs to merge documentation/ folder
+ditto docs/SwiftGodotPatterns/ docs/
+rm -rf docs/SwiftGodotPatterns
