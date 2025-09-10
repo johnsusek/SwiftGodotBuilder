f6bdcd29de012b74a32b55cf1004e214e866cc4d
John Susek <john@johnsolo.net>
2025-09-10 15:04:53 -0500

    Add patterns to workflow

--- Scripts/generate-docs.sh
+++ Scripts/generate-docs.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 swift package --allow-writing-to-directory "docs" generate-documentation --target SwiftGodotBuilder --include-extended-types --transform-for-static-hosting --hosting-base-path "SwiftGodotBuilder" --output-path "docs"
-
 swift package --allow-writing-to-directory "docs" generate-documentation --target SwiftGodotPatterns --include-extended-types --transform-for-static-hosting --hosting-base-path "SwiftGodotBuilder" --output-path "docs/SwiftGodotPatterns"
 
 # Post-process the generated docs to merge documentation/ folder
-ditto docs/SwiftGodotPatterns/ docs/
+rsync -a docs/SwiftGodotPatterns/data/documentation/swiftgodotpatterns/ docs/data/documentation/swiftgodotpatterns/
 rm -rf docs/SwiftGodotPatterns
