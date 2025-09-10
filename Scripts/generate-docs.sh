d0bf7ec3cca45d47fdc581aa0d51dbe0d0d05085
John Susek <john@johnsolo.net>
2025-09-10 15:32:30 -0500

    Merge doc sites

--- Scripts/generate-docs.sh
+++ Scripts/generate-docs.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-swift package --allow-writing-to-directory "docs" generate-documentation --target SwiftGodotBuilder --include-extended-types --transform-for-static-hosting --hosting-base-path "SwiftGodotBuilder" --output-path "docs"
-swift package --allow-writing-to-directory "docs" generate-documentation --target SwiftGodotPatterns --include-extended-types --transform-for-static-hosting --hosting-base-path "SwiftGodotBuilder" --output-path "docs/SwiftGodotPatterns"
-
-# Post-process the generated docs to merge documentation/ folder
-rsync -a docs/SwiftGodotPatterns/data/documentation/swiftgodotpatterns/ docs/data/documentation/swiftgodotpatterns/
-rm -rf docs/SwiftGodotPatterns
+swift package --allow-writing-to-directory "docs-build" generate-documentation --target SwiftGodotBuilder --include-extended-types --transform-for-static-hosting --hosting-base-path "SwiftGodotBuilder" --output-path "docs-build/SwiftGodotBuilder"
+swift package --allow-writing-to-directory "docs-build" generate-documentation --target SwiftGodotPatterns --include-extended-types --transform-for-static-hosting --hosting-base-path "SwiftGodotBuilder" --output-path "docs-build/SwiftGodotPatterns"
+rsync -a docs-build/SwiftGodotPatterns/ docs-build/SwiftGodotBuilder/
+rm -rf docs
+mv docs-build/SwiftGodotBuilder docs
